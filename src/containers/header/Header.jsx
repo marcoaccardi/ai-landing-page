4a2d9cc41b97d69d639d4a40828fa8eab432fe58
sonotype <contact.marco.accardi@gmail.com>
2022-09-13 09:54:39 +0200

    responsive header

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -4,7 +4,7 @@ import people from "../../assets/people.png";
 import ai from "../../assets/ai.png";
 function Header() {
   return (
-    <div className='gpt3__header section__padding'>
+    <div className='gpt3__header section__padding' id='home'>
       <div className='gpt3__header-content'>
         <h1 className='gradient__text'>
           Let’s Build Something amazing with GPT-3 OpenAI
@@ -15,17 +15,20 @@ function Header() {
           alteration boisterous the attachment. Party we years to order allow
           asked of.
         </p>
+
         <div className='gpt3__header-content__input'>
           <input type='email' placeholder='Your Email Address' />
           <button type='button'>Get Started</button>
         </div>
+
         <div className='gpt3__header-content__people'>
           <img src={people} alt='people' />
           <p>1,600 people requested access a visit in last 24 hours</p>
         </div>
-        <div className='gpt3__header-image'>
-          <img src={ai} alt='ai' />
-        </div>
+      </div>
+
+      <div className='gpt3__header-image'>
+        <img src={ai} alt='ai' />
       </div>
     </div>
   );
