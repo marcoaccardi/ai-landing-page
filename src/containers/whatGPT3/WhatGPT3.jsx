729a9143ebd6e5ba02c9c53c9d5a9584f7db4faf
sonotype <contact.marco.accardi@gmail.com>
2022-09-13 11:17:33 +0200

    markup and styles for whatgpt3

--- src/containers/whatGPT3/WhatGPT3.jsx
+++ src/containers/whatGPT3/WhatGPT3.jsx
@@ -1,8 +1,26 @@
 import React from "react";
 import "./whatGPT3.css";
+import Features from "../features/Features";
 
 function WhatGPT3() {
-  return <div>WhatGPT3</div>;
+  return (
+    <div className='gpt3__whatgpt3 section__margin' id='wgpt3'>
+      <div className='gpt3__whatgpt3-feature'>
+        <Features />
+      </div>
+      <div className='gpt3__whatgpt3-heading'>
+        <h1 className='gradient__text'>
+          The possibilities are beyond your imagination
+        </h1>
+        <p>Explore The Library</p>
+      </div>
+      <div className='gpt3__whatgpt3-container'>
+        <Features />
+        <Features />
+        <Features />
+      </div>
+    </div>
+  );
 }
 
 export default WhatGPT3;
